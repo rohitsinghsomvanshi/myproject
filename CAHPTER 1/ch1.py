c9699498ee2422d9a0619bc679c0e71c2d10760d
Rohit Singh <rohitsinghsomvanci00@gmail.com>
2026-03-25 21:06:32 +0530

    Added chapter 1 files

---
+++ CAHPTER 1/ch1.py
@@ -0,0 +1,88 @@
+ #python is general purpose dynamically typed high level and purely object-oriented programming langauge that support procedural .
+ #Procedural Programming Language wo language hoti hai jisme program ko procedures, functions, ya routines me divide kiya jata hai.
+ # isko "guido van rossum" ne 1991 me banaya tha.aur iska naam ek circus ke naam para rakha gaya hai jo unko bahut pasand tha "the monty python circus".
+ #isko hum bhaut asani se sikh sakte hai aur likh bhi sakte hai
+ #Python ko hum kaha kaha use karte hai
+ 
+ #Python is widely used for software development, web development, data science,
+ # artificial intelligence, machine learning, automation, and more.
+ 
+ #(1) web development server side(d jango,flask)
+
+ # (2) data science:
+#  Data science is a broader field that surrounds data engineering, 
+#  machine learning, and other fields like data analytics. Many built-in
+#  libraries make the mathematical calculations easier and efficient data analysis.
+# Libraries such as Pandas, NumPy, Matplotlib, and SciPy.
+
+ # (3)  Mobile app development
+ # (4) IOT devices
+ #(5) Artificial Inteligence and Machine Learning(TensorFlow, PyTorch, Scikit-learn, Matplotlib, Scipy)
+ # (6)Mathematics (Numpy,Pandas).
+ 
+#Python me single line comment(#) multiline comment ,start line where we can apply (''')&at the last line('''')
+
+
+#key feature of python:
+
+# Easy to learn and use
+# Interpreted language
+# Dynamically typed
+# Cross-platform compatibility
+# Object-oriented
+# Extensive library support
+# Large community support
+# Automatic memory management
+# Add of two Number:
+
+
+
+print("Hello ! I am Rohit Singh")
+
+
+
+
+a=10
+b=20               #multiline comment (''' 1233332 ''')
+c=a+b
+print(f"Result is :{a} + {b} ={c}")#jaise humko variable ko bhi show karana hai operator ke sath 
+#to hum function ke sath print kar sakte hai.
+
+
+
+#python me input lene ka tarika.
+
+# a=int(input("Enter a num"))
+# b=int(input("Enter a num"))
+# print("Result is:",a+b)
+# Type int, str,list
+
+x=10
+print(type(x))
+y=2.5
+print(type(y))
+z="Rohit"
+print(type(z))
+a=["a",4,5,6]
+print(type(a))
+
+
+#Python Literals:
+
+# python me litrals fixed ,constant hote hai jo value variable me assigned hote hai.
+#The constant value are diffrent types num,str,boolean
+
+a=10.2 #integer litrals
+print(type(a))
+print(x)
+
+# ye divide part hai jo alag trike se result dete hai
+
+a=5
+b=2
+c = a // b # floor Division (point value nahi deta hai )
+d = a / b # divide (normal divede ki trah result deta hai)
+e = a % b  # ye rimender output deta hai  
+print(c)
+print(d)
+print(e)
